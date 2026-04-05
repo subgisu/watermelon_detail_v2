455ff80600bf1c420ce0d571dd17401c8fdf7c74
subgisu <annjin4@gmail.com>
2026-04-05 18:22:59 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/-watermelon_detail_v2/',
+    base: '/watermelon_detail_v2/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
